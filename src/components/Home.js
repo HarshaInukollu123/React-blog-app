1b0defa00830973ab73a03ff0cabd8e7d8bfe994
HarshaInukollu123 <harshainukollu3@gmail.com>
2021-04-01 10:56:49 +0530

    initial commit

---
+++ src/components/Home.js
@@ -0,0 +1,27 @@
+import {useState, useEffect} from 'react';
+import BlogList from './BlogList'
+
+function Home() {
+  const [blogs, setBlogs] = useState([
+   { title: 'My new website', body: 'lorem ipsum...', author: 'harsha', id: 1 },
+   { title: 'Welcome party!', body: 'lorem ipsum...', author: 'vardhan', id: 2 },
+   { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'reddy', id: 3 }
+  ]);
+
+const handleDelete = id => {
+    const newBlogs = blogs.filter(blog => blog.id !== id);
+    setBlogs(newBlogs);
+}
+
+  useEffect(() => {
+        console.log("demo")
+       });
+
+  return (
+    <div className="home">
+      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+    </div>
+  );
+}
+
+export default Home;
